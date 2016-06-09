9d12d031e3ac919bb104f885106aefc24993df4e
hong7463 <hison7463@gmail.com>
2016-06-09 11:20:17 -0700

    pop up window

--- app/src/main/java/com/honghaisen/token/MainActivity.java
+++ app/src/main/java/com/honghaisen/token/MainActivity.java
@@ -3,6 +3,7 @@ package com.honghaisen.token;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.RemoteException;
@@ -11,10 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -28,15 +33,15 @@ import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer{
 
-    private TextView showTab;
-
     private BeaconManager beaconManager;
     private BluetoothAdapter bluetoothAdapter;
     private ProgressBar pb;
-    private TextView devName;
-    private TextView devId;
+    private Button start;
+    private Button stop;
     private final String TAG = "beacon";
     private Firebase firebase;
+    private Region region;
+    private boolean connected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +53,14 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
         firebase = new Firebase("https://sizzling-heat-7504.firebaseio.com/");
 
         //initial the components
-        showTab = (TextView)findViewById(R.id.showTab);
+        start = (Button)findViewById(R.id.start);
+        stop = (Button)findViewById(R.id.stop);
         pb = (ProgressBar)findViewById(R.id.progressBar);
-        devName = (TextView)findViewById(R.id.devName);
-        devId = (TextView)findViewById(R.id.devId);
 
         //get the beaconManager
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0118,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-        beaconManager.bind(this);
+//        beaconManager.bind(this);
 
         //check runtime permission
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -69,9 +73,44 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(beaconManager.isBound(MainActivity.this) && connected) {
+                    try {
+                        beaconManager.startRangingBeaconsInRegion(region);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else if(!beaconManager.isBound(MainActivity.this)){
+                    beaconManager.bind(MainActivity.this);
+                }
+                pb.setVisibility(View.VISIBLE);
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(beaconManager.isBound(MainActivity.this)) {
+                    try {
+                        beaconManager.stopMonitoringBeaconsInRegion(region);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                pb.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        //unbind the beaconManager
+        // unbind the beaconManager
         beaconManager.unbind(this);
     }
 
@@ -79,10 +118,11 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
     public void onBeaconServiceConnect() {
 
         //set the beacon monitor
-        Region region = new Region("myBeans", null, null, null);
+        region = new Region("myBeans", null, null, null);
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
             public void didEnterRegion(Region region) {
+                connected = true;
                 Log.d(TAG, "didEnterRegion");
                 try {
                     beaconManager.startRangingBeaconsInRegion(region);
@@ -93,16 +133,13 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
 
             @Override
             public void didExitRegion(Region region) {
+                connected = false;
                 Log.d(TAG, "didExitRegion");
                 try {
                     beaconManager.stopRangingBeaconsInRegion(region);
-                    firebase.child(getDeviceId()).setValue(getLocalBluetoothName());
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            MainActivity.this.devName.setText("");
-                            MainActivity.this.devId.setText("");
-                            MainActivity.this.showTab.setText("out of range");
                             MainActivity.this.pb.setVisibility(View.VISIBLE);
                         }
                     });
@@ -119,33 +156,46 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
 
         beaconManager.setRangeNotifier(new RangeNotifier() {
             @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, final Region region) {
                 for (final Beacon beacon : collection) {
-                    if (beacon.getDistance() <= 0.5) {
-                        firebase.child(getDeviceId()).setValue(getLocalBluetoothName());
-                        Log.d(TAG, getLocalBluetoothName());
-                        Log.d(TAG, "distance: " + beacon.getDistance() + " id1: " + beacon.getId1() + " id2: " + beacon.getId2() + " id3: " + beacon.getId3());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                MainActivity.this.devName.setText("Device name: " + getLocalBluetoothName());
-                                MainActivity.this.devId.setText("Device ID: " + getDeviceId());
-                                MainActivity.this.pb.setVisibility(View.INVISIBLE);
-                                MainActivity.this.showTab.setText("distance: " + (int)(beacon.getDistance() * 1000) / 1000.0 + " meters");
+                    Log.d(TAG, beacon.getBluetoothAddress().toString());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            pb.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    firebase.child(beacon.getBluetoothAddress()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Log.d(TAG, dataSnapshot.getValue().toString());
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("Where am I?")
+                                    .setMessage(dataSnapshot.getValue().toString())
+                                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                            try {
+                                                beaconManager.stopRangingBeaconsInRegion(region);
+                                            } catch (RemoteException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    })
+                                    .show();
+                            try {
+                                beaconManager.stopRangingBeaconsInRegion(region);
+                            } catch (RemoteException e) {
+                                e.printStackTrace();
                             }
-                        });
-                    } else {
-                        firebase.child(getDeviceId()).removeValue();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                MainActivity.this.devName.setText("");
-                                MainActivity.this.devId.setText("");
-                                MainActivity.this.pb.setVisibility(View.VISIBLE);
-                                MainActivity.this.showTab.setText("out of range");
-                            }
-                        });
-                    }
+                        }
+
+                        @Override
+                        public void onCancelled(FirebaseError firebaseError) {
+
+                        }
+                    });
                 }
             }
         });
