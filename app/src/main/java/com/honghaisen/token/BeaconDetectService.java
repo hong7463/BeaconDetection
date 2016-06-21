11351d9ce94b2bf0f81636da02eaccbc5e81c2a6
hh1141 <hh1141@nyu.edu>
2016-06-20 19:21:21 -0700

    add comment in service

--- app/src/main/java/com/honghaisen/token/BeaconDetectService.java
+++ app/src/main/java/com/honghaisen/token/BeaconDetectService.java
@@ -58,6 +58,8 @@ public class BeaconDetectService extends Service implements BeaconConsumer {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(BeaconDetectService.this, "start service", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "start service");
+
+        //bind beacon Manager to current service
         beaconManager.bind(BeaconDetectService.this);
         return super.onStartCommand(intent, flags, startId);
     }
@@ -65,11 +67,14 @@ public class BeaconDetectService extends Service implements BeaconConsumer {
     @Override
     public void onBeaconServiceConnect() {
 
+        //initialize the region for monitor, uuid = null, major = null, minor = null
         region = new Region("myBeacon", null, null, null);
 
+        //start monitor the region
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
             public void didEnterRegion(Region region) {
+                //when enter the region start ranging the nearby beacon
                 Log.d(TAG, "Service didEnterRegion");
                 try {
                     beaconManager.startRangingBeaconsInRegion(region);
@@ -80,6 +85,7 @@ public class BeaconDetectService extends Service implements BeaconConsumer {
 
             @Override
             public void didExitRegion(Region region) {
+                //when exit the region stop ranging the beacon
                 Log.d(TAG, "Service didExitRegion");
                 try {
                     beaconManager.stopRangingBeaconsInRegion(region);
@@ -94,28 +100,36 @@ public class BeaconDetectService extends Service implements BeaconConsumer {
             }
         });
 
+        //start a beacon range
         beaconManager.setRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> collection, final Region region) {
                 for (final Beacon beacon : collection) {
                     Log.d(TAG, beacon.getBluetoothAddress().toString());
                     Log.d(TAG, "in Service");
+                    //read data from fire base
                     firebase.child(beacon.getBluetoothAddress()).addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             Log.d(TAG, dataSnapshot.getValue().toString());
+
+                            //when get a data from fire base, show a notification at system tray
                             NotificationCompat.Builder builder = new NotificationCompat.Builder(BeaconDetectService.this)
                                     .setSmallIcon(R.drawable.ic_stat_name)
                                     .setContentTitle("Where an I?")
                                     .setContentText(dataSnapshot.getValue().toString());
 
+                            //Intent for click the notification
                             Intent resultIntent = new Intent(BeaconDetectService.this, MainActivity.class);
 
+                            //make sure navigate the backward will lead back to home screen
                             TaskStackBuilder stackBuilder = TaskStackBuilder.create(BeaconDetectService.this);
                             stackBuilder.addParentStack(MainActivity.class);
                             stackBuilder.addNextIntent(resultIntent);
                             PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
                             builder.setContentIntent(resultPendingIntent);
+
+                            //show the notification in system tray
                             NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
                             notificationManager.notify(1, builder.build());
                         }
@@ -129,6 +143,7 @@ public class BeaconDetectService extends Service implements BeaconConsumer {
             }
         });
 
+        //start monitor the beacons
         try {
             beaconManager.startMonitoringBeaconsInRegion(region);
         } catch (RemoteException e) {
@@ -139,11 +154,15 @@ public class BeaconDetectService extends Service implements BeaconConsumer {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        //stop monitor the beacons
         try {
             beaconManager.stopMonitoringBeaconsInRegion(region);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+
+        //unbind the beacon detection service
         beaconManager.unbind(BeaconDetectService.this);
     }
 }
